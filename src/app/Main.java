b43e68c77d263398669514ddf5985a6d31ccaa3c
Quentin <quentin.beaussart@gmail.com>
2020-10-11 20:54:39 +0200

    WIP

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,26 @@
 package app;
 import java.util.Scanner;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
+	
+	static MongoClient mongoClient;
+	static MongoDatabase database;
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		/*String uri = "mongodb://192.168.56.50:27017";
+		MongoClientURI connectionString = new MongoClientURI(uri);*/
+		mongoClient = new MongoClient("192.168.56.50");
+		
+		displayMenu();
+	}
 
 	private static void displayMenu() {
 		System.out.println("---------- MONGO APP ----------");
@@ -11,13 +30,16 @@ public class Main {
 		System.out.println("4 - Rechercher un document à partir d'un nom");
 		System.out.println("0 - Quitter l'application");
 		System.out.println("-------------------------------");
-		Scanner sc = new Scanner(System.in);
 		switch(sc.nextInt()) {
 		case 1 :
 			selectBase();
 			break;
 		case 2 :
-			selectCollection();
+			if(database != null) {
+				selectCollection();
+			} else {
+				System.out.println("Attention, vous devez d'abord sélectionner une base de données");
+			}
 			break;
 		case 3 :
 			listerDocs();
@@ -27,13 +49,10 @@ public class Main {
 			break;
 		case 0 :
 			System.out.println("Bye bye");
+			sc.close();
+			mongoClient.close();
 			break;
 		}
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		displayMenu();
 	}
 
 	private static void searchDoc() {
@@ -52,14 +71,20 @@ public class Main {
 
 	private static void selectCollection() {
 		// TODO Auto-generated method stub
-		System.out.println("selectCollection");
+		System.out.println("Entrez le nom de la collection souhaitée : ");
+		String coll = sc.next();
+		MongoCollection<Document> collection = database.getCollection(coll);
+		System.out.println("Collection \"" + collection + "\" sélectionée.");
 		displayMenu();
 		
 	}
 
 	private static void selectBase() {
 		// TODO Auto-generated method stub
-		System.out.println("selectBase");
+		System.out.println("Entrez le nom de la base de données souhaitée : ");
+		String db = sc.next();
+		database = mongoClient.getDatabase(db);
+		System.out.println("Base de données \"" + db + "\" sélectionée.");
 		displayMenu();
 		
 	}

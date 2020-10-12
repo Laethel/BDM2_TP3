dd178da9d30924eec19fda105a671a480efffe01
Quentin <quentin.beaussart@gmail.com>
2020-10-12 14:59:53 +0200

    Done

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class Main {
 	
 	static MongoClient mongoClient;
 	static MongoDatabase database;
+	static MongoCollection<Document> collection;
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		/*String uri = "mongodb://192.168.56.50:27017";
-		MongoClientURI connectionString = new MongoClientURI(uri);*/
-		mongoClient = new MongoClient("192.168.56.50");
-		
+		mongoClient = new MongoClient("192.168.56.50");	
 		displayMenu();
 	}
 
@@ -38,43 +38,71 @@ public class Main {
 			if(database != null) {
 				selectCollection();
 			} else {
-				System.out.println("Attention, vous devez d'abord sélectionner une base de données");
+				System.err.println("Attention, vous devez d'abord sélectionner une base de données");
+				displayMenu();
 			}
 			break;
 		case 3 :
-			listerDocs();
+			if(collection != null) {
+				listerDocs();
+			} else {
+				System.err.println("Attention, vous devez d'abord sélectionner une collection");
+				displayMenu();
+			}
 			break;
 		case 4 :
-			searchDoc();
+			if(collection != null) {
+				searchDoc();
+			} else {
+				System.err.println("Attention, vous devez d'abord sélectionner une collection");
+				displayMenu();
+			}
 			break;
 		case 0 :
 			System.out.println("Bye bye");
 			sc.close();
 			mongoClient.close();
 			break;
+		default :
+			System.out.println("Choix non reconnu.");
+			displayMenu();
 		}
 	}
 
 	private static void searchDoc() {
 		// TODO Auto-generated method stub
-		System.out.println("searchDoc");
+		System.out.println("Entrez le nom du document recherché : ");
+		String nomDoc = sc.next();
+		Document document = collection.find(Filters.eq("name", nomDoc)).first();
+		if(document != null) {
+	        String res = document.toJson();
+	        System.out.println("Document trouvé : " + res);
+		} else {
+			System.out.println("Document \"" + nomDoc +"\" non trouvé.");
+		}
+        String res = document.toJson();
+        System.out.println("Document trouvé : " + res);
 		displayMenu();
-		
 	}
 
 	private static void listerDocs() {
 		// TODO Auto-generated method stub
 		System.out.println("listerDocs");
+		FindIterable<Document> documents = collection.find();
+		for (MongoCursor<Document> it = documents.iterator(); it.hasNext();) {
+			Document doc = it.next();
+			System.out.println(doc.toJson());
+			System.out.println();
+		}
 		displayMenu();
-		
 	}
 
 	private static void selectCollection() {
 		// TODO Auto-generated method stub
 		System.out.println("Entrez le nom de la collection souhaitée : ");
 		String coll = sc.next();
-		MongoCollection<Document> collection = database.getCollection(coll);
-		System.out.println("Collection \"" + collection + "\" sélectionée.");
+		collection = database.getCollection(coll);
+		System.out.println("Collection \"" + collection + "\" sélectionnée.");
 		displayMenu();
 		
 	}
@@ -84,10 +112,7 @@ public class Main {
 		System.out.println("Entrez le nom de la base de données souhaitée : ");
 		String db = sc.next();
 		database = mongoClient.getDatabase(db);
-		System.out.println("Base de données \"" + db + "\" sélectionée.");
+		System.out.println("Base de données \"" + db + "\" sélectionnée.");
 		displayMenu();
-		
 	}
-	
-
 }
